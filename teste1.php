7724e77944f097bd1eda9ed16790446ab63607bc
jaime <jaime.osmar.rodrigues.filho@gmail.com>
2022-04-20 22:02:11 -0400

    novo test

---
+++ teste1.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>meu primeiro Site em PHP! WooHoo!</title>
+	</head>
+	<body>
+		<?php
+		for($i = 0; $i <10; $i++) {
+			print("linha número " . $i . "<br />");
+			}
+		?>
+	</body>
+</html>
